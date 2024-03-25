3491e9661b4aeee784e404b775c580d50a1fe36e
Admin <Admin@ADMIN-PC>
2024-03-25 16:26:40 +0700

    (loaddmhome)

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -10,24 +10,24 @@ use App\Http\Services\Product\ProductService;
 
 class MainController extends Controller
 {
-    // protected $slider;
-    // protected $menu;
-    // protected $product;
+    protected $slider;
+    protected $menu;
+    protected $product;
 
-    // public function __construct(SliderService $slider,MenuServices  $menu,
-    //     ProductService $product)
-    // {
-    //     $this->slider = $slider;
-    //     $this->menu = $menu;
-    //     $this->product = $product;
-    // }
+    public function __construct(SliderService $slider,MenuServices  $menu,
+         ProductService $product)
+    {
+       $this->slider = $slider;
+        $this->menu = $menu;
+        $this->product = $product;
+    }
 
     public function index()
     {
         return view('main', [
             'title' => 'Shop Nước Hoa ABC',
-//             'sliders' => $this->slider->show(),
-//             'menus' => $this->menu->show(),
+            'sliders' => $this->slider->show(),
+            'menus' => $this->menu->show(),
 //             'products' => $this->product->get()
        ]);
    }
