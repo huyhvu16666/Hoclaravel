eaf2a6ffb06f4101153b092f9f79c473cb2a6b5d
Admin <Admin@ADMIN-PC>
2024-03-30 21:16:56 +0700

    (loaddmsf)

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -1,4 +1,5 @@
-{{-- <title>{{ $title }}</title>
+
+<title>{{ $title }}</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
@@ -31,42 +32,4 @@
 <link rel="stylesheet" type="text/css" href="{{asset('public/teamplate/css/util.css')}}">
 <link rel="stylesheet" type="text/css" href="{{asset('public/teamplate/css/main.css')}}">
 <!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{asset('public/teamplate/css/style.css')}}">
-<meta name="csrf-token" content="{{ csrf_token() }}"> --}}
-
-
-<head>
-	<title>Home</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->
-<link rel="icon" type="image/png" href="{{asset('public/teamplate/images/icons/favicon.png')}}"/>
-<!--=====================================/==========================================================-->
-<link rel="stylesheet" type="text/css" href="{{asset('public/teamplate/vendor/bootstrap/css/bootstrap.min.css')}}">
-<!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{asset('public/teamplate/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
-<!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{asset('public/teamplate/fonts/iconic/css/material-design-iconic-font.min.css')}}">
-<!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{asset('public/teamplate/fonts/linearicons-v1.0.0/icon-font.min.css')}}">
-<!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{asset('public/teamplate/vendor/animate/animate.css')}}">
-<!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{asset('public/teamplate/vendor/css-hamburgers/hamburgers.min.css')}}">
-<!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{asset('public/teamplate/vendor/animsition/css/animsition.min.css')}}">
-<!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{asset('public/teamplate/vendor/select2/select2.min.css')}}">
-<!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{asset('public/teamplate/vendor/daterangepicker/daterangepicker.css')}}">
-<!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{asset('public/teamplate/vendor/slick/slick.css')}}">
-<!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{asset('public/teamplate/vendor/MagnificPopup/magnific-popup.css')}}">
-<!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{asset('public/teamplate/vendor/perfect-scrollbar/perfect-scrollbar.css')}}">
-<!--===============================================================================================-->
-<link rel="stylesheet" type="text/css" href="{{asset('public/teamplate/css/util.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('public/teamplate/css/main.css')}}">
-<!--===============================================================================================-->
-</head>
+<meta name="csrf-token" content="{{ csrf_token() }}">
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -8,12 +8,11 @@
                    <h1>{{ $title }}</h1>
                 </div>
 
-                
                 <div class="flex-w flex-c-m m-tb-10">
                     <div class="flex-c-m stext-106 cl6 size-104 bor4 pointer hov-btn3 trans-04 m-r-8 m-tb-4 js-show-filter">
                         <i class="icon-filter cl2 m-r-6 fs-15 trans-04 zmdi zmdi-filter-list"></i>
                         <i class="icon-close-filter cl2 m-r-6 fs-15 trans-04 zmdi zmdi-close dis-none"></i>
-                        Filter 123
+                        Filter
                     </div>
 
                     <div class="flex-c-m stext-106 cl6 size-105 bor4 pointer hov-btn3 trans-04 m-tb-4 js-show-search">
